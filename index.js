1ac8662244944ab755ac497d5afe183ac6f91920
unknown <you@example.com>
2022-05-12 00:39:49 +0600

    initial set up

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+require('dotenv').config();
+//-------------middleware-------------//
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send("Backend server is started");
+})
+app.listen(port, () => {
+    console.log('server running', port);
+})
